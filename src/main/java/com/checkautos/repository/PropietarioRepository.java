6da53aff5a39a1f2b730cd6285227167b70181df
Ander848 <andervalencia848@gmail.com>
2026-04-20 04:37:35 -0500

    ProyectoCasiTerminado

---
+++ src/main/java/com/checkautos/repository/PropietarioRepository.java
@@ -0,0 +1,20 @@
+package com.checkautos.repository;
+
+import com.checkautos.models.Propietario;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PropietarioRepository extends MongoRepository<Propietario, String> {
+
+
+    Optional<Propietario> findByCedula(String cedula);
+
+
+    boolean existsByCedula(String cedula);
+
+
+    java.util.List<Propietario> findByNombreContainingIgnoreCase(String nombre);
+}
